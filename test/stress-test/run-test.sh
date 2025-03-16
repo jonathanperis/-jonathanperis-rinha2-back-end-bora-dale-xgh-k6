fc65fea5420b56110c6886d6757e9436db58e3a6
Jonathan Peris <jperis.silva@gmail.com>
2025-03-15 22:07:30 -0300

    Update run-test.sh

--- test/stress-test/run-test.sh
+++ test/stress-test/run-test.sh
@@ -8,13 +8,6 @@ if [ "$MODE" = "prod" ]; then
     echo "Running in Production mode: generating report"
     # In production mode, run k6 tests quietly.
     k6 run rinha-test.js --quiet
-    if [ -f stress-test-report.html ]; then
-        chmod 644 stress-test-report.html
-        cp stress-test-report.html /reports/
-        echo "Report copied to /reports/stress-test-report.html"
-    else
-        echo "Report not found!"
-    fi
 elif [ "$MODE" = "dev" ] || [ -z "$MODE" ]; then
     echo "Running in Dev mode: exporting data to InfluxDB"
     # In dev mode (default) export data to InfluxDB.
